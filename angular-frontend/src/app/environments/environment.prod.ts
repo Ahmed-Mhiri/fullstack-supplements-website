24e0cdd29e523e63bd560b4d6e941b383219a1a5
Ahmed Mhiri <ahmedmhiri2004@gmail.com>
2025-06-14 16:34:23 +0200

    feat: Add Nginx reverse proxy and update app config

--- angular-frontend/src/app/environments/environment.prod.ts
+++ angular-frontend/src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api' // This is likely your current production URL
+  apiUrl: '/api' // Use a relative path
 };
--- store/src/main/java/com/supplements/store/controller/CustomerController.java
+++ store/src/main/java/com/supplements/store/controller/CustomerController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,10 +20,6 @@ import com.supplements.store.service.CustomerService;
 
 @RestController
 @RequestMapping("/api/customers")
-@CrossOrigin(origins = {
-    "http://localhost:4200",
-    "https://ahmed-mhiri.github.io"
-})
     public class CustomerController {
 
     private final CustomerService customerService;
